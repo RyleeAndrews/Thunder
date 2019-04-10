59882e0bc144674e74d0fa3cb07d9655acc2558f
Rylee Andrews <ryleeandrews73@gmail.com>
2019-04-10 17:42:12 -0600

    working on main page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,19 +5,45 @@ import {Button} from 'semantic-ui-react';
 
 
 class Home extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      clicked: false,
+    };
+  }
+
+  toggleClicked = () => {
+    this.setState({clicked: true});
+  }
+
   render(){
 
     return(
-      <div className="full">
-        <div>
-          <div className="openerwords">
-            Enter Into the World Of
-          </div>
-          <Button> I want bass </Button>
-          <div className='openbelow'>
-            Thunder
+      <div>
+        {!this.state.clicked ?
+          <div className="full">
+            <div className="openerwords">
+              Enter Into the World Of
+            </div>
+            <Button className="landingbtn" onClick={this.toggleClicked}> I want bass </Button>
+            <div className='openbelow'>
+              Thunder
+            </div>
+        </div>
+        :
+        <div className="main">
+          <div className="header">
+            <h4 className="headerWords"> Thunder </h4>
+            <Button className="mainbtn"> Home </Button>
+            <Button className="mainbtn"> About </Button>
+            <Button className="mainbtn"> Music </Button>
+            <Button className="mainbtn"> Blog </Button>
+            <Button className="mainbtn"> Photography </Button>
+            <div className="bottomBar"></div>
           </div>
         </div>
+      }
       </div>
     )
   }
