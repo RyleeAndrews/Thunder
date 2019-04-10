f215327df3e0afaf60e015c16ebec8c433691c03
Rylee Andrews <ryleeandrews73@gmail.com>
2019-04-10 16:04:43 -0600

    setting up landing page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,4 +1,6 @@
+import './home.scss';
 import React from 'react';
+import {Button} from 'semantic-ui-react';
 
 
 
@@ -6,8 +8,16 @@ class Home extends React.Component {
   render(){
 
     return(
-      <div>
-        hey
+      <div className="full">
+        <div>
+          <div className="openerwords">
+            Enter Into the World Of
+          </div>
+          <Button> I want bass </Button>
+          <div className='openbelow'>
+            Thunder
+          </div>
+        </div>
       </div>
     )
   }
