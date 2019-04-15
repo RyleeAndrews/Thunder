0b3d66944da06acfbc9a6fc9da4915c138e82e62
Rylee Andrews <ryleeandrews73@gmail.com>
2019-04-15 09:14:33 -0600

    working on home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -67,7 +67,7 @@ class Home extends React.Component {
         :
         <div className="main">
           <div className="header">
-            <Image className="headimg" src={require('../assets/facebookcoverfoto.png')} size="large"/>
+            <Image className="headimg" src={require('../assets/psy.png')} size="large"/>
             <Button className="mainbtn"> Home </Button>
             <Link to="/about">
               <Button className="mainbtn"> About </Button>
@@ -76,6 +76,15 @@ class Home extends React.Component {
             <Button className="mainbtn"> Blog </Button>
             <Button className="mainbtn"> Photography </Button>
             <div className="bottomBar"></div>
+            <iframe width="100%" height="300" scrolling="no" frameborder="no" className="soundcloud"
+            allow="autoplay"
+            title="soundcloud"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/283338997&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+            <p className="intro"> Thunder Skye Morales, 21, is a Pacific North West based producer residing in the heart of the Rocky Mountains.
+             He has been producing for about as long as he's been DJing, totaling out around three and a half years.
+             His sets usually consist of a lot of his independent work, up and coming artists, local support, doubles, chops,
+             and a little bit of greed. Thunder's typical set times run about an hour, but are jammed packed full of low frequency shakers
+             and crowd bending madness. If you aren't satisfied with the set within the immediate first fifteen minutes, dubstep obviously isn't your cup of tea. </p>
             <Image className="liveimg" src={require('../assets/live.png')} size="large"/>
           </div>
         </div>
